8628de2aa8ab909a1a1c839395d1b4e385612e08
DavinciDreams <64185677+DavinciDreams@users.noreply.github.com>
2025-04-20 00:15:55 -0400

    inex

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,10 @@
+// pages/index.jsx
+import Lobby from '../components/Lobby';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Lobby />
+    </div>
+  );
+}
